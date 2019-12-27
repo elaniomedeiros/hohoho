f3d5fe01cf71488161aaaf82f2dbead74a95d78c
elaniomedeiros <elaniomedeiros@gmail.com>
2019-12-27 07:49:54 -0300

    Create a second test with 2 as parameter.

--- test_noel.py
+++ test_noel.py
@@ -8,5 +8,7 @@ from noel import solution
 class TestNoel(unittest.TestCase):
     def test_one_Ho(self):
         self.assertEqual(solution(1), "Ho!")
+    def test_two_Ho(self):
+        self.assertEqual(solution(2), "Ho Ho!")
 
 unittest.main()
