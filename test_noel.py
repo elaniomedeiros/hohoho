1057b246819d42c05140ed2fab18ca35469a80c8
elaniomedeiros <elaniomedeiros@gmail.com>
2019-12-27 07:57:12 -0300

    create a third test with 5 as parameter .

--- test_noel.py
+++ test_noel.py
@@ -10,5 +10,7 @@ class TestNoel(unittest.TestCase):
         self.assertEqual(solution(1), "Ho!")
     def test_two_Ho(self):
         self.assertEqual(solution(2), "Ho Ho!")
+    def test_five_Ho(self):
+        self.assertEqual(solution(5), "Ho Ho Ho Ho Ho!")
 
 unittest.main()
